8123690ec57c201205fe83254bf61619fb64f9a1
kingl0w <tdytomsawyer@protonmail.com>
2023-09-11 16:01:56 -0700

    updates

--- src/lottery.ts
+++ src/lottery.ts
@@ -12,19 +12,37 @@ runButton?.addEventListener('click', () => {
 
   const didWin = game.checkTicket(player.getTicketNumbers());
 
-  updateUI(game.getWinningNumbers(), didWin);
+  //displays the winning numbers in the .balls elements
+  updateBalls(game.getWinningNumbers());
+
+  //displays only the player's numbers in the comparison_numbers element
+  updateComparisonNumbers(player.getTicketNumbers());
+
+  //displays the result message
+  updateResultMessage(didWin);
 });
 
-function updateUI(winningNumbers: number[], didWin: boolean): void {
+function updateBalls(winningNumbers: number[]): void {
   const ballElements = document.querySelectorAll('.balls');
   for (let i = 0; i < ballElements.length; i++) {
     ballElements[i].textContent = winningNumbers[i].toString();
   }
+}
 
+function updateComparisonNumbers(playerNumbers: number[]): void {
+  const comparisonElement = document.getElementById('comparison_numbers');
+  if (comparisonElement) {
+    const playerNumbersText = playerNumbers.join(' ');
+
+    comparisonElement.textContent = playerNumbersText;
+  }
+}
+
+function updateResultMessage(didWin: boolean): void {
   const resultElement = document.getElementById('result');
   if (resultElement) {
     resultElement.textContent = didWin
-      ? 'Contrats!! You won!'
+      ? 'Congratulations! You won!'
       : 'Sorry, better luck next time';
   }
 }
